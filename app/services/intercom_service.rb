84bb6d1cd82fb57a278c69819db4ccf6675e414d
ironcadiz <afcadiz@uc.cl>
2020-10-02 12:35:58 -0300

    feat(backend): Add basic data download job

--- app/services/intercom_service.rb
+++ app/services/intercom_service.rb
@@ -1,31 +1,80 @@
 class IntercomService < PowerTypes::Service.new(:token)
-  # Service code goes here
-
-  def create_contact(email)
-    intercom_client.contacts.create(email: email, role: "lead")
+  def download_conversations
+    conversations = []
+    intercom_client.conversations.all.each do |convo|
+      conversations << get_single_conversation(convo)
+    end
+    conversations
+  rescue StandardError
+    conversations
   end
 
-  def download_conversations
-    intercom_client.conversations.all.map do |convo|
-      conversation = intercom.conversations.find(id: convo.id)
-      {
-        id: conversation.id,
-        created_at: conversation.created_at,
-        tags: conversation.tags,
-        conversation_parts: conversation.conversation_parts.map do |part|
-          {
-            body: part.body,
-            id: part.id,
-            author_type: part.author_type
-          }
-        end
-      }
+  def generate_fake_leads
+    return unless Rails.env.development?
+
+    10.times do |i|
+      create_contact "fake_dude_#{i}@platan.us"
+    end
+  end
+
+  def generate_fake_messages
+    return unless Rails.env.development?
+
+    contacts = intercom_client.contacts.all
+    contacts.each do |ct|
+      intercom_client.messages.create(
+        {
+          from: {
+            type: "contact",
+            id: ct.id
+          },
+          body: "halp me pls"
+        }
+      )
     end
   end
 
   private
 
+  def get_single_conversation(convo)
+    conversation = intercom_client.conversations.find(id: convo.id)
+    {
+      id: conversation.id,
+      created_at: conversation.created_at,
+      tags: conversation.tags,
+      conversation_parts: parse_conversation_parts
+    }
+  end
+
+  def parse_conversation_parts
+    conversation_parts = [{
+      body: conversation.source.body,
+      id: conversation.source.id,
+      author_type: conversation.source.author.type,
+      created_at: conversation.created_at
+    }]
+    conversation.conversation_parts.each do |part|
+      if part.body
+        conversation_parts << {
+          body: part.body,
+          id: part.id,
+          author_type: part.author.type,
+          created_at: part.created_at
+        }
+      end
+    end
+    conversation_parts
+  end
+
+  def create_contact(email)
+    intercom_client.contacts.create(email: email, role: "lead")
+  end
+
   def intercom_client
-    @intercom_client ||= Intercom::Client.new(token: @token, api_version: '2.2')
+    @intercom_client ||= Intercom::Client.new(
+      token: @token,
+      api_version: '2.2',
+      handle_rate_limit: true
+    )
   end
 end
